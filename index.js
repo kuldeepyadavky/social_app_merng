d295f8ac4c22aa5c352785062acd6128fa2d9d82
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-07-29 11:16:30 +0530

    checking database connection

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ const mongoose = require('mongoose');
 const gql = require('graphql-tag');
 
 const { MONGOURI } = require('./config.js');
+const Post = require('./models/Post');
+const User = require('./models/User');
 
 const typeDefs = gql`
+	type Post {
+		id: ID!
+		body: String!
+		createdAt: String!
+		username: String!
+	}
 	type Query {
-		sayHi: String!
+		getPosts: [Post]
 	}
 `;
 
 const resolvers = {
 	Query: {
-		sayHi: () => {
-			'Hello World !';
+		async getPosts() {
+			try {
+				const posts = await Post.find();
+				return posts;
+			} catch (err) {
+				throw new Error(err);
+			}
 		},
 	},
 };
