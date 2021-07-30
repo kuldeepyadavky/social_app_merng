1a5d4086c68e750da4e80b0b8f37bb20c3a8288e
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-07-30 08:30:38 +0530

    setting up initial client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>MERNG SOCIAL APP</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
