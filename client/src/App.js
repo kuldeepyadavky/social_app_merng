594272286a698bc5a8cba94cb87dcccf0031a974
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-07-30 11:59:57 +0530

    setting up semantic-ui and making basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+import Login from './components/Login';
+import Home from './components/Home';
+import Register from './components/Register';
+import MenuBar from './components/MenuBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>MERNG SOCIAL APP</p>
-			</header>
-		</div>
+		<Router>
+			<Container>
+				<MenuBar />
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/register' component={Register} />
+				</Switch>
+			</Container>
+		</Router>
 	);
 }
 
